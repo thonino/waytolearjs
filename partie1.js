d951fc60c4c99637f352504b88ef7fbe8dea47a1
thonino <thoninoben@gmail.com>
2024-07-19 00:12:54 +0200

    part 1 and 2 : done

---
+++ partie1.js
@@ -0,0 +1,93 @@
+// Exercice 1:
+// retourne somme de 2 nombre.
+
+// function addition (x,y){
+//  let addition = x + y;
+//  return addition;
+// }
+// console.log('Exercice 1 : ',addition(1,2))
+// console.log('Exercice 1 : ',addition(-2,-4))
+
+
+// Exercice 2:
+// convertir minutes en secondes.
+// function minuteToSecond(minutes){
+// return minutes*60;
+// }
+// console.log('Exercice 2 : ',minuteToSecond(4));
+// console.log('Exercice 2 : ',minuteToSecond(3));
+
+
+// Exercice 3:
+// incrémente le nombre +1 et renvoie le résultat.
+// function increment(n){
+  // retourne valeur de n après y avoir ajouté 1, sans modifier la valeur de n elle-même.
+  // return n + 1;
+  // opérateur de pré-incrémentation. incrémente n de 1, retourne nouvelle valeur de n.
+  // return ++n;
+  // assignation et additionne. ajoute 1 à n, assigne résultat à n, retourne nouvelle valeur n.
+//   return n += 1;
+// }
+//   console.log('Exercice 3 : ',increment(1));
+//   console.log('Exercice 3 : ',increment(4));
+
+// Exercice 4:
+// avec base et hauteur, trouver la surface : (base * hauteur) / 2
+// function getSurface(base, hauteur){
+//   return (base * hauteur) / 2;
+// }
+// console.log("Exercice 4 : ", getSurface(8,2));
+// console.log("Exercice 4 : ", getSurface(7, 3));
+
+// Exercice 5:
+// inverser une chaîne de caractères.
+// function strReverse(str){
+//   let split =  str.split("");
+//   let tab = [];
+//   for(let i = 1; i <= split.length; i++){
+//     tab.push(split[split.length-i])
+//   }
+//   return tab.join("");
+
+//   // Prédéfini : reverse()
+//   // return str.split("").reverse().join("");
+// }
+// console.log("Exercice 5 : ", strReverse('WayToLearnX'))
+// console.log("Exercice 5 : ", strReverse('Hello'))
+
+
+// Exercice 6:
+// trouver le plus grand des trois entiers donnés.
+
+// function getMax(a, b, c){
+//   // let max = a >= b ? a : b ; 
+//   // return max > c ? max : c;
+
+// // Prédéfini : Math.max()
+// return Math.max(a, b, c);
+
+// }
+// console.log("Exercice 6 : ", getMax(5,9,1));
+// console.log("Exercice 6 : ", getMax(2,3,10));
+
+
+// Exercice 7:
+// renvoie le premier élément du tableau. 
+// function getfirst(tab){
+//   return tab[0];
+// }
+// console.log("Exercice 7 : ", getfirst([1, 2, 3]));
+// console.log("Exercice 7 : ", getfirst([50, 60, 70]));
+
+// Exercice 8:
+// récupérer l’URL d’un site Web 
+// console.log(document.URL);
+
+// Exercice 9:
+// renvoyer le reste de deux nombres. 
+// function rest(a,b){
+//   return a % b;
+// }
+// console.log("Exercice 9 : ", rest(1,3));
+// console.log("Exercice 9 : ", rest(2,4));
+// console.log("Exercice 9 : ", rest(3,3));
---
+++ partie2.js
@@ -0,0 +1,88 @@
+// Exercice 1:
+// Corrigez le code suivant (Erreur de syntaxe), pour calculer le carré.
+// function carre(b) { return $a*$a; }
+
+// function carre(b) {
+// 	return b*b;
+// }
+// console.log("Exercice 1 : ", carre(2));
+// console.log("Exercice 1 : ", carre(4));
+
+
+// Exercice 2:
+// TRUE si la somme des deux nombres est inférieure à 100. 
+
+// function check(a,b) {
+//   return a + b < 100;
+// }
+// console.log("Exercice 2 : ", check(5, 20));
+// console.log("Exercice 2 : ", check(60, 55));
+
+// Exercice 3:
+// convertir les heures en secondes. 
+
+// function hourtosecond(a){
+//   return a * 60 * 60;
+// }
+// console.log("Exercice 3 : ",hourtosecond(1));
+// console.log("Exercice 3 : ",hourtosecond(5));
+
+// Exercice 4 :
+// CTRUE s’il est inférieur ou égal à zéro, sinon retournez FALSE.
+
+// function check(n){
+//   return n <= 0;
+// }
+// console.log("Exercice 4 : ",check(1));
+// console.log("Exercice 4 : ",check(-1));
+// console.log("Exercice 4 : ",check(0));
+
+// Exercice 5
+// TRUE lorsque a est égal à b; 
+// sinon retournez FALSE.
+
+// function check(a, b){
+//   return a === b ;
+// }
+// console.log(check(3,6));
+// console.log(check(1,'1'));
+// console.log(check(1,1));
+
+// TRUE si un entier est divisible par 5, sinon retournez FALSE.
+// Exercice 6
+
+// function check(n){
+//   return n % 5 === 0;
+// }
+// console.log(check(5));
+// console.log(check(10));
+// console.log(check(6));
+
+// Exercice 7
+// Convertir (heures, minutes) en secondes.
+// function hmtos(a,b){
+//   return (a*3600) + (b*60);
+// }
+// console.log(hmtos(1, 10));
+// console.log(hmtos(0, 59));
+// console.log(hmtos(0, 0));
+
+// Exercice 8
+// function reverse(tab){
+//   let reverse = [];
+//   let length = tab.length;
+//   for (let i = 1 ; i <= length ; i++) {
+//     reverse.push(tab[length-i]);
+//   }
+//   return reverse;
+// }
+// console.log(reverse([1, 2, 3]));
+// console.log(reverse([1, 1, 2, 2, 3]));
+
+// Exercice 9
+
+// function lastindex(tab){
+//   return tab[tab.length - 1]
+// }
+// console.log(lastindex([1, 2, 3, 4]));
+// console.log(lastindex(["Alex", "Bob", "Emily"]));
